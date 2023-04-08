98f63e8513a3e18251ec966a15ca74ada0937d9e
saikathalderr <saikathalderr@gmail.com>
2023-04-08 09:26:26 +0530

    fix: socket backend connection

--- src/context/socket.ts
+++ src/context/socket.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import { Socket, io } from 'socket.io-client';
 
 const BACK_END_URL: string = String(import.meta.env.VITE_BACK_END_URL)
-export const socket: Socket = io(BACK_END_URL, {
+export const socket: Socket = io(`ws://${BACK_END_URL}`, {
   reconnection: true,
   reconnectionDelay: 500,
   reconnectionAttempts: Infinity,
