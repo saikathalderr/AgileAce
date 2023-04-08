074d6ffaf0bdef0f24332d485e9595db09a86766
saikathalderr <saikathalderr@gmail.com>
2023-04-08 09:38:26 +0530

    fix: netlify redirect & backend connection

--- src/context/socket.ts
+++ src/context/socket.ts
@@ -1,8 +1,16 @@
 import { createContext } from 'react';
 import { Socket, io } from 'socket.io-client';
 
-const BACK_END_URL: string = String(import.meta.env.VITE_BACK_END_URL)
-export const socket: Socket = io(`ws://${BACK_END_URL}`, {
+const EC2_URL: string = String(import.meta.env.VITE_BACK_END_URL)
+let BACK_END_URL: string
+
+if (import.meta.env.DEV) {
+  BACK_END_URL = `ws://${EC2_URL}`
+} else {
+  BACK_END_URL = `wss://${EC2_URL}`
+}
+
+export const socket: Socket = io(BACK_END_URL, {
   reconnection: true,
   reconnectionDelay: 500,
   reconnectionAttempts: Infinity,
